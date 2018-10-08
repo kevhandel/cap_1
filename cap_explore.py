2936b34edb78a7650bec62b23966d10320f85080
kev <kevhandel@MacBook-Pro-2.local>
2018-10-08 15:49:04 -0600

    Monday afternoon losing power

--- cap_explore.py
+++ cap_explore.py
@@ -1,12 +1,23 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime
+from datetime import date
+from datetime import time
 #%matplotlib inline
 import csv
 
+today = date.today()
+days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+#seconds = datetime.datetime.now.seconds
+now = datetime.now()
+#md = str(today.day)+'_'+str(today.month)+'_'+days[today.weekday()]+'_'+str(seconds)
+md = f'{today.day}_{today.month}_{days[today.weekday()]}_{now.microsecond}'
+
+print(md)
 years = []
 chems = []
-df_chems = pd.read_csv('~/galvanize/capstone/data/unique_chems.csv')
+df_chems = pd.read_csv('~/galvanize/capstone/1_cap/data/unique_chems.csv')
 #df_chems.plot.bar(x='Year', y='Unique_chemical', rot=0)
 fig, ax = plt.subplots()
 ax.plot(df_chems['Year'], df_chems['Unique_chemical'])
@@ -14,7 +25,7 @@ ax.plot(df_chems['Year'], df_chems['Unique_chemical'])
 fig.autofmt_xdate()
 ax.set_title('Unique Chemicals Inventoried in \n US EPA TRI by Year')
 
-plt.savefig('data/chems.png')
+plt.savefig(f'figs/chemicals_{md}.png')
 #plt.show()
 
 # print('Year', 'Unique Chemicals')
